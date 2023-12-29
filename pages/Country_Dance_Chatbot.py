315155ffa8bd3d4929d5de1faccd6f0cd8ef173b
mitchelly00 <65699298+mitchelly00@users.noreply.github.com>
2023-12-29 01:33:31 -0600

    Update Country_Dance_Chatbot.py
    added session state variable

--- pages/Country_Dance_Chatbot.py
+++ pages/Country_Dance_Chatbot.py
@@ -39,7 +39,7 @@ from htmlTemplates import css, bot_template, user_template
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI(openai_api_key)
+    llm = ChatOpenAI(st.session_state.openai_api_key)
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
@@ -84,9 +84,9 @@ def main():
 
     openai_api_key = st.text_input("Add your OpenAI API key to interact with the rulebook!")
     if openai_api_key:
-        st.session_state.open_api_key = openai_api_key
+        st.session_state.openai_api_key = openai_api_key
         #get vector store
-        embedding_function = OpenAIEmbeddings(openai_api_key=openai_api_key,model="text-search-ada-doc-001")
+        embedding_function = OpenAIEmbeddings(openai_api_key=st.session_state.openai_api_key,model="text-search-ada-doc-001")
         vectorstore = FAISS.load_local("FAISS",embedding_function)
 
         #get_conversation_chain(vectorstore)
