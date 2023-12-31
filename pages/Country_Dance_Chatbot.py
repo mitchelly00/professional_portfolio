8d2b628834fc7c65e8fe082b993f7fa38d62f9e2
mitchelly00 <65699298+mitchelly00@users.noreply.github.com>
2023-12-30 21:49:20 -0600

    Update Country_Dance_Chatbot.py
    updated text page

--- pages/Country_Dance_Chatbot.py
+++ pages/Country_Dance_Chatbot.py
@@ -85,6 +85,8 @@ def main():
 
     text3 = '''Disclaimer:
     \n - This tool is not affliated with the UCWDC. The answers are non binding. Please consult the rules [here.](https://ucwdc.org/rules/)
+    \nFeedback:
+    \n - I would love to hear how I am doing. Please give your feedback here
     '''
 
     colT1,colT2 = st.columns([1,10])
