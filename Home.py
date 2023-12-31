12db11172ee1dc3089d5b7f18f3abeba2b6a005d
mitchelly00 <65699298+mitchelly00@users.noreply.github.com>
2023-12-30 21:16:50 -0600

    Update Home.py
    Updated text

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from htmlTemplates import css, bot_template, user_template, hide_streamlit_style
 
 def main():
-    st.set_page_config(page_title="MJ Schonhoeft",
+    st.set_page_config(page_title="MJ Lopez-Schonhoeft",
                        page_icon="pics/Logo small With background 2.png")
     
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
@@ -15,9 +15,11 @@ def main():
 
     #st.markdown(streamlit_style, unsafe_allow_html=True)
     st.header("About me:")
-    st.text('I am a Data Science Analyst at Deloitte.')
-    st.text('I am passionate about creating AI applications that best serve the needs of \nend users.')
-    st.write('---')
+    text = '''I am a Data Science Analyst at Deloitte.
+
+    I am passionate about creating AI applications that best serve the needs of end users.
+    '''
+    st.markdown(text)
     st.header("Projects:")
     # st.subheader(" - UN Buget Forcasting")    
     st.subheader(" - Country Dance Chatbot")
