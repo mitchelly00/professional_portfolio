e77e9f9384ed25fbd92c2cbb32ab41fa9931f190
mitchelly00 <65699298+mitchelly00@users.noreply.github.com>
2023-12-30 21:50:20 -0600

    Update Country_Dance_Chatbot.py
    updated text layout

--- pages/Country_Dance_Chatbot.py
+++ pages/Country_Dance_Chatbot.py
@@ -87,6 +87,7 @@ def main():
     \n - This tool is not affliated with the UCWDC. The answers are non binding. Please consult the rules [here.](https://ucwdc.org/rules/)
     \nFeedback:
     \n - I would love to hear how I am doing. Please give your feedback here
+    \n 
     '''
 
     colT1,colT2 = st.columns([1,10])
