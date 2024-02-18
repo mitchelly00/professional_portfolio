7b94894c506a71d6459f828fdf088e83879a7006
mitchelly00 <mjschonhoeft@utexas.edu>
2024-02-17 23:00:40 -0600

    added an s

--- pages/2_US_Deportations_Visualized.py
+++ pages/2_US_Deportations_Visualized.py
@@ -130,7 +130,7 @@ def main():
     \n - The ANOVA statistic (F-value) compares the variability between groups to the variability within groups. A larger F-value indicates greater differences between group means relative to differences within groups. It generally takes a F value greater than 2.5 to be statistically signifigant
     \n - Since the p-value (0.2526) is greater than the alpha level (0.05), we fail to reject the null hypothesis.
     \n - The null hypothesis in ANOVA states that there are no significant differences among the group means. Therefore, in this case, we do not have sufficient evidence to conclude that there are significant differences among the group means.
-    \n In summary, based on these results, there is insufficient evidence to reject the null hypothesis. This suggests that there are no significant differences between political parties in terms of amount of deportation.
+    \n In summary, based on these results, there is insufficient evidence to reject the null hypothesis. This suggests that there are no significant differences between political parties in terms of amount of deportations.
     '''
 
     st.write(text3)
