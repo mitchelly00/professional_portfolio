097ffe786afe3543dca66da6c12a53d623ed2fe8
mitchelly00 <mjschonhoeft@utexas.edu>
2024-02-17 20:26:54 -0600

    home pics

--- Home.py
+++ Home.py
@@ -5,6 +5,8 @@ def main():
     st.set_page_config(page_title="MJ Schonhoeft",
                        page_icon="pics/Logo small With background 2.png")
     
+    st.image("pics/home_banner.png")
+    
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.write(css, unsafe_allow_html=True)
 
