e92fbf7c3c7c06d136fdcb131f6e309130c8fcf2
mitchelly00 <mjschonhoeft@utexas.edu>
2024-02-17 22:19:49 -0600

    html stuff

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -54,8 +54,8 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
             </style>"""
 
 
 
+##root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
