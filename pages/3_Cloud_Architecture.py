d9180276eb6df9c8012fcd17ce09384f2a830f94
mitchelly00 <mjschonhoeft@utexas.edu>
2024-02-22 22:54:13 -0600

    3_Cloud_Architecture

--- pages/3_Cloud_Architecture.py
+++ pages/3_Cloud_Architecture.py
@@ -17,10 +17,11 @@ def main():
     text1 = '''
     Above is the general architecture of the current web application you are viewing. 
     \n 
-    This application's general architecture is as follows:
+    This application's details about the architecture is as follows:
     \n - **EC2** the instance connects to an Application Load Balancer
     \n - **Application Load Balancer** automatically redirects all HTTP traffic to HTTPS traffic.
     \n      - The load balancer has an HTTPS Certificate through AWS Certificate Manager. 
+    \n 
     \n - **Route 53** routes DNS traffic to the Application Load Balancer
     '''
     st.markdown(text1)
@@ -33,8 +34,10 @@ def main():
     \n - **TMUX** ensures a consistant session. TMUX allows me to disconnect from a session without terminating running streamlit processes.
     \n - **NGINX** servers as a  reverse proxy to convert outward bound traffic from 8502 to port 80 (HTTP) to connect to the Application Load Balancer.
     \n - **OPENAI API** The application calls the OPENAI API to power the chatbot.
+    \n
+    \n Click [here.](https://https://github.com/mitchelly00/professional_portfolio) to learn more about the code for this project. 
     '''
-    st.markdown(text2)
+    st.markdown(text2, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()    
